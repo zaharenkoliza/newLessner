dafbbaf539edba4d2ed67dcd46c0cc6aa74083fa
zaharenkoliza <Liza93087@gmail.com>
2024-09-13 16:05:33 +0300

    adaptiv

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 const button_phone_call = document.getElementsByClassName('header-p-regular')[0];
-const form = document.getElementsByClassName('form-window')[0];
+const form = document.getElementsByClassName('form-window');
 const overlay_background = document.getElementsByClassName('overlay-background')[0];
 const overlay_close = document.getElementsByClassName('form-overlay-close')[0];
+const body = document.getElementsByTagName('body')[0];
 
 button_phone_call.addEventListener('click', () => {
 	console.log('s');
-	form.classList.add('_active');
+	form[0].classList.add('_active');
 	overlay_background.classList.add('_active-overlay-background');
 	overlay_close.classList.add('_active-overlay-close');
-	console.log(overlay_background.classList);
+	body.classList.add('overflow-hidden');
+	form[0].classList.remove('hidden');
 });
 
 overlay_close.addEventListener('click', () => {
-	form.classList.remove('_active');
+	form[0].classList.remove('_active');
+	form[1].classList.add('hidden');
 	overlay_background.classList.remove('_active-overlay-background');
 	overlay_close.classList.remove('_active-overlay-close');
+	body.classList.remove('overflow-hidden');
 });
+
+function formIsSubmitted() {
+	form[0].classList.add('hidden');
+	form[1].classList.remove('hidden');
+}
