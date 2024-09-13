708a97d0960cd9beae30cae73ffb80b50b9c8fba
zaharenkoliza <Liza93087@gmail.com>
2024-09-13 18:04:46 +0300

    burger-button + form for mobile version + clear out

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ const form = document.getElementsByClassName('form-window');
 const overlay_background = document.getElementsByClassName('overlay-background')[0];
 const overlay_close = document.getElementsByClassName('form-overlay-close')[0];
 const body = document.getElementsByTagName('body')[0];
+const phone_number = document.getElementById('header-phone-number');
 
-button_phone_call.addEventListener('click', () => {
-	console.log('s');
+function openFormWindow() {
 	form[0].classList.add('_active');
 	overlay_background.classList.add('_active-overlay-background');
 	overlay_close.classList.add('_active-overlay-close');
 	body.classList.add('overflow-hidden');
 	form[0].classList.remove('hidden');
-});
+}
+
+button_phone_call.addEventListener('click', openFormWindow);
 
 overlay_close.addEventListener('click', () => {
 	form[0].classList.remove('_active');
@@ -25,3 +27,18 @@ function formIsSubmitted() {
 	form[0].classList.add('hidden');
 	form[1].classList.remove('hidden');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	function moveElement() {
+		const viewportWidth = window.innerWidth;
+		if (viewportWidth <= 1024) {
+			phone_number.addEventListener('click', openFormWindow);
+		} else {
+			phone_number.removeEventListener('click', openFormWindow);
+		}
+	}
+		
+	moveElement();
+	
+	window.addEventListener('resize', moveElement);
+ });
